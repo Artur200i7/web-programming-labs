3e1104cc4db0d6c4dde104ba1b48e7c32ca9908d
Artur200i7 <shumejkoartur@gmail.com>
2026-04-03 02:47:35 +0300

    Lab 1: Task 1 - Destructuring and spread/rest

---
+++ lab1/src/task-5/data.js
@@ -0,0 +1,9 @@
+export const LIBRARY_NAME = "Central City Library";
+
+export const books = [
+  { title: "Кобзар", author: "Тарас Шевченко", year: 1840, pages: 280, genre: "poetry" },
+  { title: "Clean Code", author: "Robert Martin", year: 2008, pages: 464, genre: "programming" },
+  { title: "Місто", author: "Валер'ян Підмогильний", year: 1928, pages: 320, genre: "novel" },
+  { title: "1984", author: "George Orwell", year: 1949, pages: 328, genre: "dystopia" },
+  { title: "JavaScript: The Good Parts", author: "Douglas Crockford", year: 2008, pages: 176, genre: "programming" },
+];
