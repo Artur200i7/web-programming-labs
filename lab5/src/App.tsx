2f96e4a58180a394679d7e5d1d2ca98a75d56ddb
Artur200i7 <shumejkoartur@gmail.com>
2026-04-06 22:53:19 +0300

    Lab 5: Add todo update and delete

--- lab5/src/App.tsx
+++ lab5/src/App.tsx
@@ -1,5 +1,30 @@
+import { useQuery } from "@tanstack/react-query"
+import { todosApi } from "./api/todos"
+import type { Todo } from "./types/todo"
+
 function App() {
-  return <div>App works</div>
+  const { data, isLoading, isError, error } = useQuery<Todo[]>({
+    queryKey: ["todos"],
+    queryFn: () => todosApi.getAll(),
+  })
+
+  if (isLoading) return <div>Завантаження...</div>
+
+  if (isError) return <div>Помилка: {(error as Error).message}</div>
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Todo List</h1>
+
+      {data!.map((todo) => (
+        <div key={todo.id}>
+          <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+            {todo.title}
+          </span>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default App
