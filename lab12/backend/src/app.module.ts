88e0fad99b5af32cbedb707e7da95a07117cf058
Artur200i7 <shumejkoartur@gmail.com>
2026-06-11 04:03:53 +0300

    Lab 12: Add Docker Compose configuration

--- lab12/backend/src/app.module.ts
+++ lab12/backend/src/app.module.ts
@@ -16,7 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   database: process.env.DB_NAME || 'postgres',
   autoLoadEntities: true,
   synchronize: true,
-});
+})
 
 
     // Додай свої модулі сюди
